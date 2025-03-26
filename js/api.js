fccfcf867d2d26f2aa115c59236a68eaaa9244dc
Denis Cheremiskin <cheremiskindenis@gmail.com>
2025-03-27 00:16:04 +0700

    Добавляет букву k в API_URL

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://31.javascript.htmlacademy.pro/kestagram';
+const API_URL = 'https://31.javascript.htmlacademy.pro/kekstagram';
 
 const fetchData = async () => {
   const response = await fetch(`${API_URL}/data`);
